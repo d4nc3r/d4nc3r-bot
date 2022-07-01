ec96f4d6b1649d80803485ce127459f1d10f9770
Jamie M <7iny.d4nc3r@gmail.com>
2022-07-01 15:06:35 -0400

    inital commit, super basic bot

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
+
+client.on('ready', () => {
+  console.log(`logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', msg => {
+  switch(msg.content) {
+    case 'ping':
+      msg.reply('pong');
+      break;
+  }
+});
+
+client.login(process.env.TOKEN);
