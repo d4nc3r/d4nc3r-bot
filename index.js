821ff05906474f0a0313263298f33c887e566258
Jamie M <7iny.d4nc3r@gmail.com>
2022-07-01 15:35:14 -0400

    added dotenv to read env vars

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
 
